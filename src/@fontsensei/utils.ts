91360c29b8d678e41e056bfa7d6a00ec1d128c4f
Mr Bird <168046335+mrbirddev@users.noreply.github.com>
2024-10-10 15:21:03 +0800

    fix removing spaces

--- src/@fontsensei/utils.ts
+++ src/@fontsensei/utils.ts
@@ -1,7 +1,9 @@
+import {uniq} from "lodash-es";
+
 export const tagToUrlSlug = (tag: string) => {
   return tag.toLowerCase().split(' ').join('-');
 };
 export const fontFamilyToUrlParam = (name: string) => name.replace(/ /g, '+');
-export const textToUrlParam = (text?: string) => text ? [
-  ...new Set([...text.replace(/ /g, ' ')])
-].join('') : undefined;
+export const textToUrlParam = (text?: string) => text ? uniq(
+  text.replace(/ /g, '').split('')
+).join('') : undefined;
