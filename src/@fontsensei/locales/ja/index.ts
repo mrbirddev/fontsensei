98655c3fcf818049cef3fac4767ad42fc84cfe04
Mr Bird <168046335+mrbirddev@users.noreply.github.com>
2024-10-10 12:59:51 +0800

    force translation

--- src/@fontsensei/locales/ja/index.ts
+++ src/@fontsensei/locales/ja/index.ts
@@ -6,6 +6,9 @@ import tagValueMsg from "./tagValueMsg";
 import i18nMsg from "./i18nMsg";
 import landingMsg from "./landingMsg";
 
+// @ts-expect-error force translation here.
+tagValueMsg.fude = '毛筆体';
+
 export default {
   "product": product,
   "modal": modal,
