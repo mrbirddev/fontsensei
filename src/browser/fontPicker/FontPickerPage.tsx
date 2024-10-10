195b4d1304e237e6b61e8615ffb07df4d657af35
Mr Bird <168046335+mrbirddev@users.noreply.github.com>
2024-10-10 10:30:35 +0800

    fix initial font

--- src/browser/fontPicker/FontPickerPage.tsx
+++ src/browser/fontPicker/FontPickerPage.tsx
@@ -11,7 +11,7 @@ import React, {
   type PropsWithChildren
 } from 'react';
 import LandingLayout from "../../shared/ui/LandingLayout";
-import {type TagValueMsgLabelType, useCurrentLocale, useI18n, useScopedI18n} from "@fontsensei/locales";
+import {LocaleStr, type TagValueMsgLabelType, useCurrentLocale, useI18n, useScopedI18n} from "@fontsensei/locales";
 import {useRouter} from "next/router";
 import Link from "next/link";
 import Head from "next/head";
@@ -41,6 +41,29 @@ interface PageProps {
   countByTags: Record<string, number>;
   firstFontByTags: Record<string, string>;
 }
+
+const getDefaultTag = (currentLocale: LocaleStr) => {
+  switch (currentLocale) {
+    case "ja":
+      return "lang_ja";
+    case "zh-cn":
+      return "lang_zh-Hans";
+    case "zh-tw":
+      return "lang_zh-Hant";
+    case "ko":
+      return "lang_ko";
+    default:
+      return "all";
+  }
+}
+
+const getTagValue = (raw_tagValue: string | undefined, currentLocale: LocaleStr) => {
+  if (raw_tagValue) {
+    return raw_tagValue;
+  }
+
+  return getDefaultTag(currentLocale);
+};
 
 const TagButton = (props: PropsWithChildren<{
   isActive: boolean,
@@ -79,28 +102,8 @@ const FontPickerPage = (props: PageProps) => {
   const tTagValueMsg = useScopedI18n('tagValueMsg');
   const router = useRouter();
   const raw_tagValue = router.query.slugList?.[0];
-  const defaultTag = useMemo(() => {
-    switch (currentLocale) {
-      case "ja":
-        return "lang_ja";
-      case "zh-cn":
-        return "lang_zh-Hans";
-      case "zh-tw":
-        return "lang_zh-Hant";
-      case "ko":
-        return "lang_ko";
-      default:
-        return "all";
-    }
-  }, [currentLocale]);
-
-  const tagValue = useMemo(() => {
-    if (raw_tagValue) {
-      return raw_tagValue;
-    }
-
-    return defaultTag;
-  }, [defaultTag, raw_tagValue]);
+  const defaultTag = useMemo(() => getDefaultTag(currentLocale), [currentLocale]);
+  const tagValue = useMemo(() => getTagValue(raw_tagValue, currentLocale), [raw_tagValue, currentLocale]);
 
   const tagDisplayName = useMemo(
     () => tTagValueMsg(tagValue as TagValueMsgLabelType),
@@ -203,7 +206,10 @@ const FontPickerPage = (props: PageProps) => {
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const slugList = context.params?.slugList ?? [];
-  const tagValue = Array.isArray(slugList) ? slugList[0] : undefined;
+  const tagValue = getTagValue(
+    Array.isArray(slugList) ? slugList[0] : undefined,
+    (context.locale ?? "en") as LocaleStr
+  );
 
   const initialFontItemList = await listFonts({
     tagValue: tagValue,
