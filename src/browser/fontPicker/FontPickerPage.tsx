ef65f06ed630682470fa20bd9b56d0aac4cee368
Mr Bird <168046335+mrbirddev@users.noreply.github.com>
2024-10-13 15:35:15 +0800

    use modal for dropdown since it cannot be closed easily under mobile

--- src/browser/fontPicker/FontPickerPage.tsx
+++ src/browser/fontPicker/FontPickerPage.tsx
@@ -86,6 +86,9 @@ const Navbar = (props: {fullWidth?: boolean, style?: React.CSSProperties }) => {
     ];
   }, [lang, router.pathname, navbarContext?.extraMenuItems, preferredLocale]);
 
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return <>
     <div className="h-16" />
     <div className="fixed left-0 top-0 right-0 z-10" style={props.style}>
@@ -125,9 +128,15 @@ const Navbar = (props: {fullWidth?: boolean, style?: React.CSSProperties }) => {
                 }
               })}
             </div>
-            <div className="dropdown dropdown-end block md:hidden">
-              <div tabIndex={0} role="button" className="btn btn-ghost"><FaBars /></div>
-              <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+            <div className="block md:hidden">
+              <div tabIndex={0} role="button" className="btn btn-ghost" onClick={() => {
+                setIsMenuOpen(!isMenuOpen);
+              }}><FaBars /></div>
+            </div>
+            <dialog id="my_modal_1" className={"modal " + (isMenuOpen ? 'modal-open' : '')}>
+              <div className="modal-box text-gray-700 w-11/12 max-w-5xl">
+                <ModalTitle onCancel={() => setIsMenuOpen(false)}>
+                </ModalTitle>
                 {menuItems.map(item => {
                   const {icon, label, className, href, target, onClick} = item;
                   if (href) {
@@ -142,8 +151,8 @@ const Navbar = (props: {fullWidth?: boolean, style?: React.CSSProperties }) => {
                     </div>
                   }
                 })}
-              </ul>
-            </div>
+              </div>
+            </dialog>
             {/*<div className="dropdown dropdown-end">*/}
             {/*  <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">*/}
             {/*    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>*/}
