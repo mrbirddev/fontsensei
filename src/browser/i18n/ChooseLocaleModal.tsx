5a27f663a29a29dcd162c26e1656ad9731bb6a0e
Mr Bird <168046335+mrbirddev@users.noreply.github.com>
2024-10-30 13:11:09 +0800

    hacked this bug

--- src/browser/i18n/ChooseLocaleModal.tsx
+++ src/browser/i18n/ChooseLocaleModal.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import locales from "./locales";
 import {useChangeLocale, useScopedI18n} from "@fontsensei/locales";
 import {ModalTitle} from "@fontsensei/components/modal/commonComponents";
-import Link from "next/link";
 import {useRouter} from "next/router";
 import useUserPreferencesStore from "../page/useUserPreferencesStore";
 import MobileOnlyModal from "@fontsensei/components/modal/MobileOnlyModal";
@@ -12,7 +11,6 @@ const ChooseLocaleModal = (props: {
   setOpen: (o: boolean) => void,
 }) => {
   const tI18nMsg = useScopedI18n('i18nMsg');
-  const changeLocale = useChangeLocale();
   const router = useRouter();
 
   return (
@@ -22,11 +20,17 @@ const ChooseLocaleModal = (props: {
       </ModalTitle>
       <div className="flex flex-wrap justify-start items-start gap-6">
         {locales.map(item => {
-          // the href is for Google spiders
-          return <Link key={item.locale} className="link link-ghost" href={router.asPath} locale={item.locale} onClick={(e) => {
+          /*
+            TODO WHY???
+            use <a> not next/link because of this bug
+            https://github.com/vercel/next.js/issues/72063
+          */
+          return <a key={item.locale} className="link link-ghost" href={'/' + item.locale + router.asPath} onClick={() => {
             useUserPreferencesStore.getState().setLocale(item.locale);
             props.setOpen(false);
-          }}>{item.lang}</Link>
+          }}>
+            {item.lang}
+          </a>
         })}
       </div>
     </MobileOnlyModal>
