96c9b9dea0b75d9f8e32b6bfe5389cb240acd59e
FireIsGood <fireisadequate@gmail.com>
2024-12-19 01:48:30 -0800

    fix i18n of "Feedback"

--- src/pages/tag/[[...slugList]].tsx
+++ src/pages/tag/[[...slugList]].tsx
@@ -3,13 +3,11 @@ import {NavbarContext} from "../../browser/fontPicker/FontPickerPage";
 import React from "react";
 import {FontPickerPageContext} from "@fontsensei/components/fontPickerCommon";
 import {getExtraMenuItems, Toolbar} from "../../browser/fontPicker/landingComponents";
-import {useI18n} from "@nextutils/locales";
 
 export {getServerSideProps} from "../../browser/fontPicker/FontPickerPage";
 export default (props: Parameters<typeof FontPickerPage>[0]) => {
-  const t = useI18n();
   return <NavbarContext.Provider value={{
-    extraMenuItems: getExtraMenuItems(t),
+    extraMenuItems: getExtraMenuItems(),
   }}>
     <FontPickerPageContext.Provider value={{
       Toolbar: Toolbar,
