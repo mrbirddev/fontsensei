b79c3b85a23b0f172535de0d1d723fb5dc5a9c81
Mr Bird <168046335+mrbirddev@users.noreply.github.com>
2025-01-22 17:44:10 +0800

    add import

--- src/@fontsensei/locales/ja/index.ts
+++ src/@fontsensei/locales/ja/index.ts
@@ -1,12 +1,14 @@
 import tagValueMsg from "./tagValueMsg";
 import tagDescMsg from "./tagDescMsg";
 import landingMsg from "./landingMsg";
+import indexFaq from "!!raw-loader!./indexFaq.md";
 
 // @ts-expect-error force translation here.
 tagValueMsg.fude = '毛筆体';
 
 export default {
   "landingMsg": landingMsg,
+  "indexFaq": indexFaq,
   "tagValueMsg": tagValueMsg,
   "tagDescMsg": tagDescMsg,
 } as const;
