1a94b98133767103752ad23eb1281181595cad8c
Mr Bird <168046335+mrbirddev@users.noreply.github.com>
2025-01-22 17:08:33 +0800

    raw loader is still needed

--- src/@fontsensei/locales/en/index.ts
+++ src/@fontsensei/locales/en/index.ts
@@ -1,9 +1,11 @@
 import tagValueMsg from "./tagValueMsg";
 import tagDescMsg from "./tagDescMsg";
 import landingMsg from "./landingMsg";
+import indexFaq from "!!raw-loader!./indexFaq.md";
 
 const en = {
   "landingMsg": landingMsg,
+  "indexFaq": indexFaq,
   "tagValueMsg": tagValueMsg,
   "tagDescMsg": tagDescMsg,
 } as const;
